cb621cf547e786250f85be5ee97ea8529a8e0489
Emma Shen <104966589+e-shen2022@users.noreply.github.com>
2024-01-21 22:47:41 -0800

    cybernews api logic & controller

--- src/main/java/com/nighthawk/spring_portfolio/Main.java
+++ src/main/java/com/nighthawk/spring_portfolio/Main.java
@@ -2,9 +2,12 @@ package com.nighthawk.spring_portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 // @SpringBootApplication annotation is the key to building web applications with Java https://spring.io/projects/spring-boot
 @SpringBootApplication
+@ComponentScan("com.nighthawk.spring_portfolio.mvc.cybersecurity") 
+
 public class Main {
 
     // Starts a spring application as a stand-alone application from the main method
